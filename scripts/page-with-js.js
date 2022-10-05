5b6d34118b105afeb443e50c3f37122a3ad81e34
Dimterion <dimterion59@gmail.com>
2022-10-05 11:12:20 +0200

    Added font change function and styling for JS page

--- scripts/page-with-js.js
+++ scripts/page-with-js.js
@@ -11,3 +11,16 @@ if (isDarkScheme) {
   colorSchemeStatus.innerText =
     "The page is displayed in light mode based on your system preferences";
 }
+
+// Changing the font size
+let toggleFontChange = false;
+const body = document.getElementsByTagName("body")[0];
+
+function changeFontSize() {
+  toggleFontChange = !toggleFontChange;
+  if (toggleFontChange) {
+    body.style.fontSize = "1.25rem";
+  } else {
+    body.style.fontSize = "1rem";
+  }
+}
