2e6809182f49911978902ec8c4f40efbdb1efadb
Dmitrii <dimterion59@gmail.com>
2024-07-20 22:37:10 +0200

    add fetch request to weather page

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,6 +1,17 @@
 export default function getWeather() {
   const city = document.getElementById("cityInput").value;
+  const url = ``;
   console.log(city);
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weatherInfo = document.getElementById("weatherInfo");
+      weatherInfo.innerHTML = `<p>${data}</p>`;
+    })
+    .catch((error) => {
+      console.error("Error.");
+    });
 }
 
 const btn = document.getElementById("getWeather");
