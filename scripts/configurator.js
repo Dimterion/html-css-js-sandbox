1624918054d9c0bc7bae90f2513e0ffb5d31af7b
Dmitrii <dimterion59@gmail.com>
2024-11-01 12:37:05 +0100

    add btn select functionality to configurator

--- scripts/configurator.js
+++ scripts/configurator.js
@@ -1,4 +1,27 @@
 const topBar = document.querySelector("#top-bar");
+const exteriorColorSection = document.querySelector("#exterior-buttons");
+const interiorColorSection = document.querySelector("#interior-buttons");
+const exteriorImage = document.querySelector("exterior-image");
+const interiorImage = document.querySelector("interior-image");
+
+// Color selection
+const handleColorButtonClick = (event) => {
+  let button;
+
+  if (event.target.tagName === "IMG") {
+    button = event.target.closest("button");
+  } else if (event.target.tagName === "BUTTON") {
+    button = event.target;
+  }
+
+  if (button) {
+    const buttons = event.currentTarget.querySelectorAll("button");
+
+    buttons.forEach((btn) => btn.classList.remove("btn-selected"));
+
+    button.classList.add("btn-selected");
+  }
+};
 
 // Turn on/off top bar on scroll
 const handleScroll = () => {
@@ -10,3 +33,5 @@ const handleScroll = () => {
 
 // Event listeners
 window.addEventListener("scroll", () => requestAnimationFrame(handleScroll));
+exteriorColorSection.addEventListener("click", handleColorButtonClick);
+interiorColorSection.addEventListener("click", handleColorButtonClick);
