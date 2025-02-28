170f94d8a5d0bb3346ec11142a47ac9c46acc67c
Dmitrii <dimterion59@gmail.com>
2025-02-28 12:30:57 +0100

    add displayMatches function to search page

--- scripts/search.js
+++ scripts/search.js
@@ -5,7 +5,7 @@ const prom = fetch(endpoint)
   .then((data) => data.json())
   .then((data) => cities.push(...data));
 
-  console.log(cities)
+console.log(cities);
 
 function findMatches(wordToMatch, cities) {
   return cities.filter((place) => {
@@ -14,3 +14,22 @@ function findMatches(wordToMatch, cities) {
     return place.city.match(regex) || place.department.match(regex);
   });
 }
+
+function displayMatches() {
+  const matchArray = findMatches(this.value, cities);
+  const html = matchArray
+    .map((place) => {
+      return `<li>
+    <span class="name">${place.city}, ${place.department}</span>
+    </li>`;
+    })
+    .join("");
+
+  suggestions.innerHTML = html;
+}
+
+const searchInput = document.querySelector(".search");
+const suggestions = document.querySelector(".suggestions");
+
+searchInput.addEventListener("change", displayMatches);
+searchInput.addEventListener("keyup", displayMatches);
