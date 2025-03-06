338ebc4696b2631446fe9c741f12d761a57abefa
Dmitrii <dimterion59@gmail.com>
2025-03-06 11:45:58 +0100

    add initial mouseMoveShadow mechanic

--- scripts/compilation.js
+++ scripts/compilation.js
@@ -2,3 +2,17 @@
 range.oninput = () =>
   document.body.style.setProperty("--position", range.value + "%");
 // /\ Images comparison /\
+
+// \/ Mouse move shadow \/
+const mouseMoveShadowDiv = document.querySelector(".mouseMoveShadow-div");
+const mouseMoveShadowH3 = document.querySelector(".mouseMoveShadow-h3");
+
+function mouseMoveShadow(e) {
+  const { offsetWidth: width, offsetHeight: height } = mouseMoveShadowDiv;
+  let { offsetX: x, offsetY: y } = e;
+
+  console.log(x, y);
+}
+
+mouseMoveShadowDiv.addEventListener("mousemove", mouseMoveShadow);
+// /\ Mouse move shadow /\
