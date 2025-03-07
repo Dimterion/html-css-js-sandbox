37f85ae2ec9d629594d8cb82c1245ae096b419a8
Dmitrii <dimterion59@gmail.com>
2025-03-07 12:13:49 +0100

    adjust mouseMoveShadow element mechanic

--- scripts/compilation.js
+++ scripts/compilation.js
@@ -6,12 +6,21 @@ range.oninput = () =>
 // \/ Mouse move shadow \/
 const mouseMoveShadowDiv = document.querySelector(".mouseMoveShadow-div");
 const mouseMoveShadowH3 = document.querySelector(".mouseMoveShadow-h3");
+const distance = 100;
 
 function mouseMoveShadow(e) {
   const { offsetWidth: width, offsetHeight: height } = mouseMoveShadowDiv;
   let { offsetX: x, offsetY: y } = e;
 
-  console.log(x, y);
+  if (this !== e.target) {
+    x = x + e.target.offsetLeft;
+    y = y + e.target.offsetTop;
+  }
+
+  const xDistance = Math.round((x / width) * distance - distance / 2);
+  const yDistance = Math.round((y / height) * distance - distance / 2);
+
+  mouseMoveShadowH3.style.textShadow = `${xDistance}px ${yDistance}px red`;
 }
 
 mouseMoveShadowDiv.addEventListener("mousemove", mouseMoveShadow);
