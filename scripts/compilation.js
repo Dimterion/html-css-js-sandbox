0b24e28d97bd33cf4cda7d4f4ad9ee9e97d589a0
Dmitrii <dimterion59@gmail.com>
2025-03-09 12:36:07 +0100

    add expandable elements mechanic

--- scripts/compilation.js
+++ scripts/compilation.js
@@ -28,3 +28,41 @@ function mouseMoveShadow(e) {
 
 mouseMoveShadowDiv.addEventListener("mousemove", mouseMoveShadow);
 // /\ Mouse move shadow /\
+
+// \/ Expandable article \/
+const expandableElements = document.querySelectorAll("[data-expandable]");
+const expandableButtons = document.querySelectorAll("[data-expand-button]");
+
+checkForOverflow();
+expandableButtons.forEach((button) => {
+  button.addEventListener("click", toggleText);
+  setExpandButtonText(button);
+});
+
+function checkForOverflow() {
+  expandableElements.forEach((expandableElement) => {
+    if (expandableElement.classList.contains("expanded")) return;
+
+    const expandableText =
+      expandableElement.querySelector("[data-expand-text]");
+    const overflowing =
+      expandableText.scrollHeight > expandableText.clientHeight;
+
+    expandableElement.dataset.overflow = overflowing;
+  });
+}
+
+function toggleText(e) {
+  const expandableElement = e.target.closest("[data-expandable]");
+
+  expandableElement.classList.toggle("expanded");
+  setExpandButtonText(e.target);
+}
+
+function setExpandButtonText(button) {
+  const expandableElement = button.closest("[data-expandable]");
+  const expanded = expandableElement.classList.contains("expanded");
+
+  button.innerText = expanded ? "Read Less" : "Read More";
+}
+// /\ Expandable article /\
