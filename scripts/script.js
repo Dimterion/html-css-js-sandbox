cb4b081c3c072c9cd199acaf490c14c5ef931339
Dmitrii <dimterion59@gmail.com>
2025-03-12 11:55:21 +0100

    adjust index page links

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,24 @@
-const pages = ["compilation", "gallery"];
+const pages = [
+  "Animations",
+  "Compilation",
+  "Configurator",
+  "Gallery",
+  "GitHub-landing-page",
+  "InteractiveTable",
+  "ScrollSnap",
+  "Search",
+  "Tasks",
+  "ThemeSwitcher",
+];
 const indexNav = document.getElementById("index-nav");
 
 pages.forEach((page) => {
   const indexA = document.createElement("a");
   indexA.innerText = page;
+
+  indexA.classList.add("index-a");
+
+  indexA.setAttribute("href", `../pages/${page.toLowerCase()}.html`);
 
   indexNav.appendChild(indexA);
 });
