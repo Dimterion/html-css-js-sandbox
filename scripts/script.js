5e39abaa827b64d593670906d22ff188d2030b31
Dmitrii <dimterion59@gmail.com>
2025-03-12 12:17:37 +0100

    adjust index page styling

--- scripts/script.js
+++ scripts/script.js
@@ -14,6 +14,7 @@ const indexNav = document.getElementById("index-nav");
 
 pages.forEach((page) => {
   const indexA = document.createElement("a");
+  
   indexA.innerText = page;
 
   indexA.classList.add("index-a");
