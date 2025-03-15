664966425707e1bdfff5adb3d0629dd09c90bfd1
Dmitrii <dimterion59@gmail.com>
2025-03-15 11:52:24 +0100

    adjust drumKit page script

--- scripts/drumKit.js
+++ scripts/drumKit.js
@@ -1,7 +1,24 @@
-window.addEventListener("keydown", function (e) {
+function playSound(e) {
   const audio = this.document.querySelector(`audio[data-key="${e.key}"]`);
+  const key = this.document.querySelector(`.key[data-key="${e.key}"]`);
 
   if (!audio) return;
 
+  audio.currentTime = 0;
+
   audio.play();
-});
+
+  key.classList.add("playing");
+}
+
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+
+  this.classList.remove("playing");
+}
+
+const keys = document.querySelectorAll(".key");
+
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+
+window.addEventListener("keydown", playSound);
