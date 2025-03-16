3e171ffd1e8c4c993181ad4bafff1560ff3abfb5
Dmitrii <dimterion59@gmail.com>
2025-03-16 11:27:11 +0100

    adjust drumKit keys

--- scripts/drumKit.js
+++ scripts/drumKit.js
@@ -1,23 +1,23 @@
 function playSound(e) {
-  const audio = this.document.querySelector(`audio[data-key="${e.key}"]`);
-  const key = this.document.querySelector(`.key[data-key="${e.key}"]`);
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
 
   if (!audio) return;
+
+  key.classList.add("playing");
 
   audio.currentTime = 0;
 
   audio.play();
-
-  key.classList.add("playing");
 }
 
 function removeTransition(e) {
   if (e.propertyName !== "transform") return;
 
-  this.classList.remove("playing");
+  e.target.classList.remove("playing");
 }
 
-const keys = document.querySelectorAll(".key");
+const keys = Array.from(document.querySelectorAll(".key"));
 
 keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
 
